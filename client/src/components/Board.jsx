c3ee60573561d9b3606260cc4894881d6edeb5f1
Erin Turgut <erintur@meta.com>
2025-06-20 12:38:34 -0700

    can now click on board

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -4,15 +4,14 @@ const Board = ({boardData, handleDeleteBoard}) => {
 
     const navigate = useNavigate();
 
-    return <div className="board">
+    return <div className="board" onClick={() => {
+                //need to navigate to CardsPage when we click on the board
+                navigate(`/boards/${boardData.id}`, { state: {boardName: `${boardData.title}`}})
+            }}>
         <img src={boardData.image_url}/>
         <h2>{boardData.title}</h2>
         <p>{boardData.description}</p>
         <div>
-            <button onClick={() => {
-                //need to navigate to CardsPage when we click 'View Board'
-                navigate(`/boards/${boardData.id}`, { state: {boardName: `${boardData.title}`}})
-            }}>View Board</button>
             <button onClick={() => {
                 handleDeleteBoard(boardData.id) //callback to HomePage so that deleted board can be rendered visually 
             }}>Delete Board</button>
